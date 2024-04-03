693a8957d3c17640a84ad1b715927c9f2843d1df
rubenruizc <ruben.ruiz@iesnervion.es>
2024-04-03 14:37:32 +0200

    Ejercicio 3 T6

--- src/ejercicio03/Perecedero.java
+++ src/ejercicio03/Perecedero.java
@@ -3,4 +3,15 @@ package ejercicio03;
 public class Perecedero {
 
 	private int díasACaducar;
+
+
+
+
+
+	public String toString() {
+		 String infoProducto = "";
+		 infoProducto = "Nombre: " + this.díasACaducar;
+		 return infoProducto;
+	}
+	
 }
